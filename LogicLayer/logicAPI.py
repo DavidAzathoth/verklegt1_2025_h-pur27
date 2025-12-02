eab30e9c32ef12e80249aebf78dd56aacf4ff612
AdamAtli <adamatlisandgreen@gmail.com>
2025-12-02 14:23:19 +0000

    ..

---
+++ LogicLayer/logicAPI.py
@@ -0,0 +1,16 @@
+from LogicLayer.TeamLogic import Teamlogic
+from StorageLayer.storageApi import DataAPI
+
+
+class LogicAPI:
+    def __init__(self, Teamlogic: Teamlogic, DataAPI: DataAPI):
+        self.__Teamlogic = Teamlogic
+        self.__DataAPI = DataAPI
+
+        return
+
+    def createteam(self, input: dict):
+        return self.__Teamlogic.createteam(input)
+    
+    def temp(self):
+        return DataAPI.loadTeams()
