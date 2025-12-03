ac20ef9c9135eed3d0af4dedf1c2d052ee962fa4
AdamAtli <adamatlisandgreen@gmail.com>
2025-12-03 14:49:03 +0000

    bla

--- adamdebugPC.py
+++ adamdebugPC.py
@@ -6,23 +6,9 @@ from Models.TeamCaptain import TeamCaptain
 from Models.Player import Player
 from StorageLayer.storageApi import DataAPI
 from Models.ViewTeamsMenu import ShowTeams
-da=DataAPI()
-streg=''
-teams=da.loadTeams()
-tostring=[]
-print(teams)
-if teams:
-    teamlist=[]
-    for team in teams:
-        teamid=team.get('teamID')
-        teamname=team.get('teamName')
-        roster=team.get('roster')
-        wins=team.get('wins')
-        losses=team.get('losses')
-        teamlist.append(Team(teamid,teamname,roster,wins,losses,None))
-    for team in teamlist:
-        tostring.append(team.createCSVString())
 
-else:
-    print('File not found')
-print(f"Invalid input. Valid options are: {". ".join(valid_lower)}")
+
+
+listi = input()
+minnlisti = listi.split()
+LogicAPI.createtournament(minnlisti)
