86a338829f7e2a8a6f28cc9eace529e05247262e
Davíd Örvar Elíasson <44362281+DavidAzathoth@users.noreply.github.com>
2025-12-03 19:12:09 +0000

    tournament creation possible through logic api

--- StorageLayer/storageHandler.py
+++ StorageLayer/storageHandler.py
@@ -7,29 +7,32 @@ class StorageHandler:
         try:
             with open(file,'r') as file:
                 csv_reader = csv.DictReader(file)
-                test=csv_reader.fieldnames
                 for row in csv_reader:
                     ret_list.append(row)
             file.close()
         except FileNotFoundError:
             return False
         return ret_list
+
+
     def saveFile(self, addition, type):
+
         if type=='teams':
             loaded=self.retrieveFile('StorageLayer/Data/testfile.csv')
             try:
-                with open('StorageLayer/Data/testfile.csv','r') as file:
+                with open('StorageLayer/Data/testfile.csv','r') as file: #We need to open the file first to get fieldnames for the DictReader
                     csvreader=csv.DictReader(file)
                     keys=csvreader.fieldnames
                 file.close()
                 with open('StorageLayer/Data/testfile.csv','w') as file:
-                    #keys = loaded[0].keys()
                     csvwriter=csv.DictWriter(file, keys)
                     csvwriter.writeheader()
                     csvwriter.writerows(loaded)
                     csvwriter.writerow(addition)
+                file.close()
             except FileNotFoundError:
                 return False
+            
         if type=='tournaments':
             loaded=self.retrieveFile('StorageLayer/Data/tournaments.csv')
             try:
@@ -38,10 +41,10 @@ class StorageHandler:
                     keys=csvreader.fieldnames
                 file.close()
                 with open('StorageLayer/Data/tournaments.csv','w') as file:
-                    #keys = csv.DictReader(file).fieldnames()
                     csvwriter=csv.DictWriter(file, keys)
                     csvwriter.writeheader()
                     csvwriter.writerows(loaded)
                     csvwriter.writerow(addition)
+                file.close()
             except FileNotFoundError:
                 return False
