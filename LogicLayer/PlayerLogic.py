ed6bf5eecb5168d89dd951907a35736d2801b937
AdamAtli <adamatlisandgreen@gmail.com>
2025-12-04 21:20:57 +0000

    .

---
+++ LogicLayer/PlayerLogic.py
@@ -0,0 +1,18 @@
+from StorageLayer.storageApi import DataAPI
+from Models.Player import Player
+
+class PlayerLogic:
+    def __init__(self, dataApi: DataAPI):
+        self.__dataApi = dataApi
+        pass
+
+    def createplayer(self, player: list):
+        teamID = player[0]
+        playerGamertag = player[1]
+        fullname = player[2]
+        phoneNumber = player[3]
+        emailAddress = player[4]
+        address = player[5]
+        link = player[6]
+        dateOfBirth = player[7]
+        self.__dataApi.savePlayer()
